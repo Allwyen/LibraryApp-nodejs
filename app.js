ae255d8702be6d1fd8feb6a31cb4a0d9298b56a8
allwyen <allwyen27@gmail.com>
2019-08-28 22:40:38 +0530

    update with mongodb atlas

--- app.js
+++ app.js
@@ -9,7 +9,27 @@ app.use(Express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-Mongoose.connect("mongodb://localhost:27017/LMS");
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+Mongoose.connect("mongodb+srv://mongodb:mongodb@mycluster-ucvz5.mongodb.net/LMS?retryWrites=true&w=majority");
+//Mongoose.connect("mongodb://localhost:27017/LMS");
 
 const BookModel = Mongoose.model("book",{
     title:String,
@@ -110,7 +130,8 @@ author=[{
 }];
 
 app.post('/insertbook',(req,res)=>{
-    var result = BookModel.save((error,data)=>{
+    var book = new BookModel(req.body);
+    var result = book.save((error,data)=>{
         if(error)
         {
             throw error;
