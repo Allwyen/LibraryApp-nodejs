e9dbd208fb168f5e5d4e305498c3dfb752c1aace
Allwyen <allwyen27@gmail.com>
2019-08-29 10:25:25 +0530

    updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://libraryappangular.herokuapp.com' );
+    res.setHeader('Access-Control-Allow-Origin', 'https://libraryappangular.herokuapp.com' );
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
